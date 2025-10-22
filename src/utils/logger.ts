0936f1b6dccef6cfd3b65cf3bc0986f7fca8b6ae
Tamizmani05 <tamizhmani13270@gmail.com>
2025-10-22 21:49:57 +0530

    feat: Added Colorette

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,22 +5,36 @@ import config from '../configs/config';
 import { EApplicationEnvironment } from '../constants/application';
 import path from 'path';
 import * as sourceMapSupport from 'source-map-support';
+import { blue, green, magenta, red, yellow } from 'colorette';
 
 // Trace Support
 sourceMapSupport.install();
 
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level);
+        case 'INFO':
+            return blue(level);
+        case 'WARN':
+            return yellow(level);
+        default:
+            return level;
+    }
+};
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta } = info;
-    const customLevel = level.toUpperCase();
-    const customTimestamp = timestamp;
+    const customLevel = colorizeLevel(level.toUpperCase());
+    const customTimestamp = green(timestamp as string);
     const customMessage = message;
     const customMeta = util.inspect(meta, {
         showHidden: false,
-        depth: null
+        depth: null,
+        colors: true
     });
 
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${'META'} ${customMeta}\n`;
+    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${magenta('META')} ${customMeta}\n`;
 
     return customLog;
 });
@@ -28,7 +42,7 @@ const consoleLogFormat = format.printf((info) => {
 const fileLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta = {} } = info;
     const logMeta: Record<string, unknown> = {};
-    
+
     for (const [key, value] of Object.entries(meta as Record<string, unknown>)) {
         if (value instanceof Error) {
             logMeta[key] = {
@@ -78,5 +92,5 @@ export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...FileTransport(),...consoleTransport()]
+    transports: [...FileTransport(), ...consoleTransport()]
 });
